455086a7808dba9a871208a6e0a24023bcf0befd
ariq-azmain <ariq-azmain123@gmail.com>
2026-05-22 19:22:52 +0600

    update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,8 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/q(.*)'
+  '/q(.*)',
+  '/api/webhooks(.*)',   // ← এটা যোগ করো
 ])
 
 export default clerkMiddleware(async (auth, req) => {
