275037c8d316f6a0e3e5a568390c1cc417a0a6d7
ariq-azmain <ariq-azmain123@gmail.com>
2026-05-25 09:20:17 +0600

    add vercle analytecs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
     subsets: ["latin"],
@@ -44,6 +45,7 @@ export default function RootLayout({
                     }}
                 >
                     {children}
+                    <Analytics/>
                 </ClerkProvider>
             </body>
         </html>
